7b861f1d82940008ded738938265ae4992f12e8e
Connie <leungcnie@gmail.com>
2020-11-17 21:40:55 +0000

    Wrote input event listener that updates character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,18 @@
 $(document).ready(() => {
-
+  // Target textarea
   const $textArea = $('textarea#tweet-text');
 
+  // Input event listener
   const charCounter = function() {
     const input = $(this).val();
-    const inputLength = input.length;
+    const charsLeft = 140 - input.length;
 
-    console.log(140 - inputLength);
+    const div = $(this).parent().children()[2];
+    const counter = $(div).children()[1]; // Get counter DOM node
+    $(counter).text(charsLeft);
   }
 
+  // Calls charCounter on input event
   $textArea.on('input', charCounter);
-
-
 
 })
