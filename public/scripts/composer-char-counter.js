c375c0d9330f5542e6919adbb238c1577566842f
Connie <leungcnie@gmail.com>
2020-11-17 22:20:39 +0000

    Add conditional to add invalid counter CSS to <head> only when character count is < 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,20 @@
 $(document).ready(() => {
-  // Target textarea
+
   const $textArea = $('textarea#tweet-text');
 
   // Input event listener
-  const charCounter = function() {
+  const charCounter = function () {
     const input = $(this).val();
     const charsLeft = 140 - input.length;
+
+    // Create link tag to invalid red counter CSS
+    const link = $('<link>').attr({ 'rel': 'stylesheet', 'href': '/styles/invalid-counter.css', 'type': 'text/css' });
+
+    if (charsLeft < 0) {
+      $('head').append(link);
+    } else {
+      $("[href='/styles/invalid-counter.css']").remove();
+    }
 
     const div = $(this).parent().children()[2];
     const counter = $(div).children()[1]; // Get counter DOM node
