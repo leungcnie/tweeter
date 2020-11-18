654bb98399044c865deac978432e80be30c331cd
Connie <leungcnie@gmail.com>
2020-11-18 03:06:41 +0000

    Use jQuery .css method to change counter to red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,18 +7,15 @@ $(document).ready(() => {
     const input = $(this).val();
     const charsLeft = 140 - input.length;
 
-    // Create link tag to invalid red counter CSS
-    const link = $('<link>').attr({ 'rel': 'stylesheet', 'href': '/styles/invalid-counter.css', 'type': 'text/css' });
-
-    if (charsLeft < 0) {
-      $('head').append(link);
-    } else {
-      $("[href='/styles/invalid-counter.css']").remove();
-    }
-
     const div = $(this).parent().children()[2];
     const counter = $(div).children()[1]; // Get counter DOM node
     $(counter).text(charsLeft);
+
+    if (charsLeft < 0) {
+      $(counter).css('color', 'red')
+    } else {
+      $(counter).css('color', '#545149')
+    }
   }
 
   // Calls charCounter on input event
