95da90d7011b3b9a13945dd34502a658910d548f
Connie <leungcnie@gmail.com>
2020-11-19 04:48:57 +0000

    Add comments for clarity

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,10 +7,12 @@ $(document).ready(() => {
     const input = $(this).val();
     const charsLeft = 140 - input.length;
 
+    // Get counter element
     const div = $(this).parent().children()[2];
-    const counter = $(div).children()[1]; // Get counter DOM node
+    const counter = $(div).children()[1];
     $(counter).text(charsLeft);
 
+    // If characters over limit, display red counter
     if (charsLeft < 0) {
       $(counter).css('color', 'red');
     } else {
